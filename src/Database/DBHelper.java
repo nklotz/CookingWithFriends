516c89233b33f56b306e2a607fdf21a8cb2c9953
Hannah Acheson-Field <hannah_acheson-field@brown.edu>
2013-04-24 21:35:24 -0400

    added get kitchen and store kitchen methods to db

--- src/Database/DBHelper.java
+++ src/Database/DBHelper.java
@@ -24,7 +24,10 @@ import com.mongodb.MongoException;
 public class DBHelper implements DBHelperInterface{
 	private Mongo mongo_;
 	private DBCollection userCollection_;
-	private DB userDb_;
+	private DB userDB_;
+	private DB kitchenDB_;
+	private DBCollection kitchenCollection_;
+	
 	public DBHelper(){
 		String s = "mongod --port 27017 -dbpath /home/hacheson/course/cs032/MongoData/";
 		String[] args = s.split(" ");
@@ -36,8 +39,10 @@ public class DBHelper implements DBHelperInterface{
 		}
 		try {
 			mongo_ = new Mongo("localhost", 27017);
-			userDb_ = mongo_.getDB("users");
-			userCollection_ = userDb_.getCollection("userCollection");
+			userDB_ = mongo_.getDB("users");
+			userCollection_ = userDB_.getCollection("userCollection");
+			kitchenDB_ = mongo_.getDB("kitchens");
+			kitchenCollection_ = kitchenDB_.getCollection("kitchenCollection");
 		} catch (UnknownHostException e) {
 			System.err.println("ERROR: Could not connect to mongodb, unknown host.");
 			e.printStackTrace();
@@ -79,18 +84,51 @@ public class DBHelper implements DBHelperInterface{
 
 	@Override
 	public Kitchen getKitchen(String id) {
+		System.out.println("Making object in db helper get kitchen.");
+		BasicDBObject searchQuery = new BasicDBObject();
+		searchQuery.put("id", id);
+		DBCursor cursor = kitchenCollection_.find(searchQuery);
+		
+		while (cursor.hasNext()) {
+			 //System.out.println(cursor.next().get("account"));
+			System.out.println(cursor.next());
+		}
 		return null;
 	}
 
 	@Override
 	public void storeKitchen(Kitchen k) {
-		//TODO: Make sure username is unique.
-		
+		BasicDBObject document = new BasicDBObject();
+		document.put("id", "/k/0");
+		document.put("kitchen", "sample kitchen");
+		BasicDBObject searchQuery = new BasicDBObject();
+		searchQuery.put("id", k.getId());
+		//Adds it if it doesn't exist  currently.
+		if(kitchenCollection_.find(searchQuery).length() == 0){
+			kitchenCollection_.insert(document);
+		}
 	}
 	
+	/**
+	 * Creates a new, unique kitchen id by finding if an id is in the db already.
+	 * @return String id The new, unique kitchen id.
+	 */
 	public String createKitchenId(){
-		//TODO: Generate number, then see if /k/number is in db, and keep doing
-		//that until find a unique number.
-		return null;
+		//Generate a random number and see if it's not in the db already.
+		while(true){
+			int num = (int)(Math.random()*1000000);
+			BasicDBObject searchQuery = new BasicDBObject();
+			String id = "/k/" + num;
+			searchQuery.put("id", id);
+			if(searchQuery.isEmpty()){
+				return id;
+			}
+		//	DBCursor cursor = kitchenCollection_.find(searchQuery);
+		//	if(!cursor.hasNext()){
+		//		return id;
+		//	}	
+		}
 	}
+	
+	
 }
