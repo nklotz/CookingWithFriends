43a82feff45f02b6de7898e1edb5114fc89c7a0a
Edward Friedman <esfriedm@cs.brown.edu>
2013-05-05 14:54:29 -0400

    starting message board

--- src/GUI2/Controller.java
+++ src/GUI2/Controller.java
@@ -1400,5 +1400,30 @@ public class Controller extends AnchorPane implements Initializable {
 			}
 		}
 	}
+	
+	public void displayMessages(String newText){
+		eventCommentDisplayField.setText("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?");
+		//TODO: thissss
+	}
+	
+	public void postMessage(){
+		String post = eventCommentWriteField.getText();
+		eventCommentWriteField.setText("");
+		String pre = "";
+		String mid = "";
+		String email = _account.getID();
+		String id = email.split("@")[0];
+		if (!_account.getName().equals("")){
+			mid = id + " (" + _account.getName() + "): ";
+		} else {
+			mid = id +": ";
+		}
+		if (eventCommentDisplayField.getText().length() != 0){
+			pre = eventCommentDisplayField.getText() + "\n";
+		}
+		eventCommentDisplayField.setText(pre + mid + post);
+		String forServer = mid+post;
+		//TODO: post forServer to server
+	}
 
 }
